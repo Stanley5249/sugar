a051fe193965eebb073d7c7b889ce6b74efff4f8
Stanley5249 <seer852741@gmail.com>
2024-09-23 01:07:19 +0800

    build: change version to 0.0.1.dev0
    release manually to PyPI

--- src/sugar/__init__.py
+++ src/sugar/__init__.py
@@ -10,7 +10,7 @@ from sugar._action import Build, Converter, Event, Store, auto
 from sugar._app import ArgumentApp, CommandApp, Meta, sugar
 from sugar._parser import ArgumentParser, CommandParser
 
-__version__ = "0.1.0"
+__version__ = "0.0.1.dev0"
 
 __all__ = [
     "ArgumentApp",
